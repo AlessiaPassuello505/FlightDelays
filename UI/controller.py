e95dfaf7e19e8163811fa778ba1058a8aa319818
Ale <s322062@studenti.polito.it>
2026-05-11 11:22:59 +0200

    lez lun

--- UI/controller.py
+++ UI/controller.py
@@ -8,10 +8,17 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
+    def handleanalizza(self, e):
         name = self._view.txt_name.value
         if name is None or name == "":
             self._view.create_alert("Inserire il nome")
             return
         self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
         self._view.update_page()
+
+    def handleAerConnessi(self,e):
+        pass
+
+    def handlecerca(self,e):
+
+        pass
--- UI/view.py
+++ UI/view.py
@@ -6,36 +6,54 @@ class View(ft.UserControl):
         super().__init__()
         # page stuff
         self._page = page
-        self._page.title = "Template application using MVC and DAO"
+        self._page.title = "Tdp flights manager 2026"
         self._page.horizontal_alignment = 'CENTER'
         self._page.theme_mode = ft.ThemeMode.DARK
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
+        self.txt_Cmin = None
+        self.btn_analizzaAer = None
+
         self.txt_result = None
         self.txt_container = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("Tdp flights manager 2026", color="blue", size=24)
         self._page.controls.append(self._title)
+
+
 
         #ROW with some controls
         # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
+        self.txt_Cmin = ft.TextField(
+            label="N min compagnie",
             width=200,
-            hint_text="Insert a your name"
+
         )
 
         # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
+        self.btn_analizzaAer = ft.ElevatedButton(text="Analizza Aeroporti", on_click=self._controller.handleanalizza)
+        row1 = ft.Row([self.txt_Cmin, self.btn_analizzaAer],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
+
+        self._ddAeroportoP=ft.Dropdown(label="Aeroporto di Partenza")
+        self._btnAeroportiConnessi=ft.ElevatedButton(text="Aeroporti Connessi", on_click=self._controller.handleAerConnessi)
+        row2=ft.Row([self._ddAeroportoP, self._btnAeroportiConnessi],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row2)
+
+        self._ddAeroportoA=ft.Dropdown(label="Aeroporto di Arrivo")
+        self._txtTratteMax=ft.TextField(label="Num tratte Max")
+        self._btnCercaItinerario=ft.ElevatedButton(text="Cerca itinerario", on_click=self._controller.handlecerca)
+
+        row3=ft.Row([self._ddAeroportoA, self._txtTratteMax, self._btnAeroportiConnessi],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row3)
+
 
         # List View where the reply is printed
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
